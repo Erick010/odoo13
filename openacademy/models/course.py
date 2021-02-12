2bb088364d782a00b68d75bb4a413e8431a15665
Erick01 <50059979+Erick010@users.noreply.github.com>
2021-02-12 00:40:39 -0600

    model

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,12 +1,13 @@
-from odoo import models, fields, api, exceptions
+# -- coding: utf-8 --
+from odoo import models, fields, api
 
 
 class Course(models.Model):
     _name = 'openacademy.course'
-    _description = "Cursos de OpenAcademy "
+    _description = "OpenAcademy Courses"
 
-    name = fields.Char(String="Title", required=True)
-    dscription = fields.Text()
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
 
     responsible_id = fields.Many2one(
         'res.users', ondelete='set null', string="Responsible", index=True)
