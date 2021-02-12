8d55981532805df3a07fe3cd5ce03aef15305299
Erick01 <50059979+Erick010@users.noreply.github.com>
2021-02-11 23:57:58 -0600

    Módulo curso, manifest, demo, init , controllers

---
+++ openacademy/models/course.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api, exceptions
+
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "Cursos de OpenAcademy "
+
+    name = fields.Char(String="Title", required=True)
+    dscription = fields.Text()
+
+    responsible_id = fields.Many2one('res.users',
+                                     ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
